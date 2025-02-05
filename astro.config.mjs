fb9bf5a00e309793f5d8e9d20faf5d5c279807c3
senseiGai <swerss564@gmail.com>
2025-02-06 00:28:15 +0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+
+
+export default defineConfig({
+    integrations: [react(), svelte(), tailwind()],
+});
