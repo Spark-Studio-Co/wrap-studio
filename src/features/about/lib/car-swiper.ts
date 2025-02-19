a74cb1062b611f5c4488a6faf3fe01ef30886ee0
senseiGai <swerss564@gmail.com>
2025-02-19 15:58:23 +0500

    feat: swiper

--- src/features/about/lib/car-swiper.ts
+++ src/features/about/lib/car-swiper.ts
@@ -1,11 +1,16 @@
 import about01 from '/public/images/about01.webp';
-import about02 from '/public/images/about02.webp';
+import mercedes01 from '/public/images/mercedes01.webp'
+import genesis01 from '/public/images/genesis01.webp'
+import santafe01 from '/public/images/santafe01.webp'
+import sportage01 from '/public/images/sportage01.webp'
+import tucson01 from '/public/images/tucson01.webp'
 
 
 export const carImages = [
     about01,
-    about02,
-    about01,
-    about02,
-    about01,
+    mercedes01,
+    genesis01,
+    santafe01,
+    sportage01,
+    tucson01
 ];
